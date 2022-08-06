addd19a6429559b7fe1b7c47b4916f399a8d26c4
lt <465275024@qq.com>
2022-08-06 19:34:12 +0800

    feat: 初始化，添加eslint和stylelint

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <div class="ss"
+      v-for="item in items"
+      :key="item.id"
+    >
+      {{ item }}
+      <div class="bb">
+        bb
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const items = ref([1,2,3,4])
+
+</script>
+
+<style lang="scss">
+.ss {
+  font-size: 100px;
+}
+
+.bb {
+  font-size: 10px;
+  color: #fff;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt'
+
+const a = 1
+console.log(a)
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+
+})
